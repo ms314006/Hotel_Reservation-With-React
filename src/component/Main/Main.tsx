cbf942f53f21beb3575ca7a3c21f1a8bb64fcf9c
GQSM <ms314006@hotmail.com>
2019-08-13 22:57:48 +0800

    [INIT] Initial project

---
+++ src/component/Main/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+const Main = (props: any) => {
+  return (
+    <Button data-testid="button" variant="contained" color="secondary">
+      Primar
+    </Button>
+  );
+};
+
+export default Main;
