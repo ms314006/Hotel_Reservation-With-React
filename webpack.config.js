df462f6275b0afab94695cc4c957e9d9053ad449
GQSM <ms314006@hotmail.com>
2019-08-17 12:53:01 +0800

    [ADD] 增加對 SCSS 的開發環境

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: ['@babel/polyfill', './src/index.tsx'],
@@ -28,9 +29,49 @@ module.exports = {
             presets: ['@babel/typescript', '@babel/preset-react', '@babel/preset-env'],
           },
         },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoader: 1,
+              localIdentName: '[path]___[name]__[local]___[hash:base64:5]',
+            },
+          }
+        ],
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoader: 1,
+              localIdentName: '[path]___[name]__[local]___[hash:base64:5]',
+            },
+          },
+          {
+            loader: 'sass-loader',
+          }
+        ],
       }
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: true ? './dist/[name].css' : './dist/[name].[hash].css',
+    })
+  ],
   devServer: {
     port: 8080,
   },
