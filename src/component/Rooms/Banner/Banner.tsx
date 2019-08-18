df33e4f2ed9dd30be154ed445c10dfa043ea644e
GQSM <ms314006@hotmail.com>
2019-08-18 18:08:58 +0800

    [CHANGE] 移除多餘程式碼

--- src/component/Rooms/Banner/Banner.tsx
+++ src/component/Rooms/Banner/Banner.tsx
@@ -1,27 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import Logo from '../../Logo';
 import CommunityLink from '../../Infomation/CommunityLink';
 import Infomation from '../../Infomation/Infomation';
 import styles from './index.scss';
 
-const Banner = (props: any) => {
-  const {} = props;
-  return (
-    <div
-      className={styles.bannerBlock}
-      style={{
-        backgroundImage: 'url(https://images.unsplash.com/photo-1519710889408-a67e1c7e0452?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80)',
-      }}
-    >
-      <Logo />
-      <div className={styles.hotelInfoBlock}>
-        <CommunityLink />
-        <div className={styles.infoBorder}>
-          <Infomation />
-        </div>
+const Banner = () => (
+  <div
+    className={styles.bannerBlock}
+    style={{
+      backgroundImage: 'url(https://images.unsplash.com/photo-1558976825-6b1b03a03719?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80)',
+    }}
+  >
+    <Logo />
+    <div className={styles.hotelInfoBlock}>
+      <CommunityLink />
+      <div className={styles.infoBorder}>
+        <Infomation />
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Banner;
